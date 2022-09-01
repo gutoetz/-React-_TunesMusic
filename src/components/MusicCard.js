f7220bb91e9cea5578a2ae23cac3432b42906594
Augusto Etz <augustoetz@hotmail.com>
2022-09-01 15:45:21 -0300

    finalizando projeto

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,9 +1,43 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Carregando from '../pages/Carregando';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      carregando: false,
+      checked: false,
+    };
+  }
+
+  componentDidMount() {
+    this.alreadyFav();
+  }
+
+  alreadyFav = () => {
+    const { check } = this.props;
+    if (check) { this.setState({ checked: true }); }
+  };
+
+  favoriteChange = async (event) => {
+    const { checked } = event.target;
+    this.setState({ carregando: true, checked });
+    const { musica } = this.props;
+    if (checked) {
+      await addSong(musica);
+    } else {
+      await removeSong(musica);
+    }
+    this.setState({ carregando: false });
+  };
+
   render() {
-    const { trackName, previewUrl } = this.props;
+    const { carregando, checked } = this.state;
+
+    const { trackName, previewUrl, trackId } = this.props;
+    if (carregando) return <Carregando />;
     return (
       <div>
         <p>{trackName}</p>
@@ -15,12 +49,24 @@ export default class MusicCard extends React.Component {
           <code>audio</code>
           .
         </audio>
+        <label htmlFor={ `checkbox-music-${trackId}` }>
+          Favorita
+          <input
+            data-testid={ `checkbox-music-${trackId}` }
+            type="checkbox"
+            onChange={ this.favoriteChange }
+            checked={ checked }
+          />
+
+        </label>
       </div>
     );
   }
 }
 
 MusicCard.propTypes = {
+  musica: PropTypes.shape({}).isRequired,
   previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
   trackName: PropTypes.string.isRequired,
 };
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Carregando from './Carregando';
 
 class Album extends React.Component {
   constructor(props) {
@@ -11,28 +13,44 @@ class Album extends React.Component {
       artist: '',
       albumName: '',
       musics: [],
+      artworkUrl100: '',
+      carregando: false,
+      favoritas: [],
     };
   }
 
   componentDidMount() {
     this.requisitandoMusicas();
+    this.pegandoFavoritas();
   }
 
   requisitandoMusicas = async () => {
     const { match: { params: { id } } } = this.props;
     const musicas = await getMusics(id);
-    const { artistName, collectionName } = musicas[0];
-    this.setState({ artist: artistName, albumName: collectionName, musics: musicas });
-    console.log(musicas);
+    const { artistName, collectionName, artworkUrl100 } = musicas[0];
+    this.setState({ artist: artistName,
+      albumName: collectionName,
+      musics: musicas,
+      artworkUrl100 });
+  };
+
+  pegandoFavoritas = async () => {
+    this.setState({ carregando: true });
+    const favoritas = await getFavoriteSongs();
+    console.log(favoritas);
+    this.setState({ favoritas, carregando: false });
   };
 
   render() {
-    const { artist, albumName, musics } = this.state;
+    const { artist, albumName, musics, artworkUrl100,
+      carregando, favoritas } = this.state;
+    if (carregando) { return <Carregando />; }
     return (
       <div data-testid="page-album">
         <Header />
         <div>
           <section>
+            <img src={ artworkUrl100 } alt={ albumName } />
             <h1 data-testid="artist-name">{artist}</h1>
             <h3 data-testid="album-name">{albumName}</h3>
           </section>
@@ -40,9 +58,15 @@ class Album extends React.Component {
             {musics.length === 0 ? (null) : (
               musics.filter((musica) => musica.kind === 'song').map((musica) => (
                 <MusicCard
+                  musica={ musica }
                   key={ musica.trackId }
                   trackName={ musica.trackName }
                   previewUrl={ musica.previewUrl }
+                  trackId={ musica.trackId }
+                  check={
+                    favoritas.filter((music) => (
+                      music.trackId === musica.trackId)).length !== 0
+                  }
                 />
               ))
             )}
